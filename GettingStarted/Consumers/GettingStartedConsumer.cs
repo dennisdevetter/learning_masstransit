34d989c85fe48a9614b9f2a192f7199c9edcf974
Dennis De Vetter <dennis.de.vetter@telenet.be>
2023-03-14 08:23:38 +0100

    init

---
+++ GettingStarted/Consumers/GettingStartedConsumer.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Logging;
+
+namespace Company.Consumers
+{
+    using System.Threading.Tasks;
+    using MassTransit;
+    using Contracts;
+
+    public class GettingStartedConsumer :
+        IConsumer<HelloMessage>
+    {
+        readonly ILogger<GettingStartedConsumer> _logger;
+
+        public GettingStartedConsumer(ILogger<GettingStartedConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<HelloMessage> context)
+        {
+            _logger.LogInformation($"Hello {context.Message.Name}");
+            return Task.CompletedTask;
+        }
+    }
+}
