c4cd3061c7584920605df9e58a744bfa24ba6d7d
Dennis De Vetter <dennis.de.vetter@telenet.be>
2023-03-15 18:50:23 +0100

    projects

--- GettingStarted/Program.cs
+++ GettingStarted/Program.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
-using LearningMassTransit.Infrastructure;
+using LearningMassTransit.Consumers;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,11 +14,51 @@ namespace GettingStarted
             await CreateHostBuilder(args).Build().RunAsync();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) => { ConfigureApplication(services); });
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) => { ConfigureApplication(services, hostContext); });
 
-        private static void ConfigureApplication(IServiceCollection services)
+        private static void ConfigureApplication(IServiceCollection services, HostBuilderContext hostBuilderContext)
         {
-            services.ConfigureMassTransit();
+            var configuration = hostBuilderContext.Configuration;
+
+            ConfigureMassTransit(services, configuration);
+        }
+
+        private static void ConfigureMassTransit(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.SetKebabCaseEndpointNameFormatter();
+
+                // By default, sagas are in-memory, but should be changed to a durable
+                // saga repository.
+                x.SetInMemorySagaRepositoryProvider();
+
+                x.AddConsumers(typeof(GettingStartedConsumer).Assembly);
+
+                var useRabbitMq = configuration.GetValue<bool>("Masstransit:UseRabbitMq");
+
+                if (useRabbitMq)
+                {
+                    x.UsingRabbitMq((context, cfg) =>
+                    {
+                        cfg.Host("localhost", "/", h => {
+                            h.Username("guest");
+                            h.Password("guest");
+
+                        });
+                        cfg.ConfigureEndpoints(context);
+                    });
+                }
+
+                else
+                {
+                    x.UsingInMemory((context, cfg) =>
+                    {
+                        cfg.ConfigureEndpoints(context);
+                    });
+                }
+
+            });
         }
     }
 }
---
+++ LearningMassTransit.Processor.Api/Program.cs
@@ -0,0 +1,66 @@
+using GettingStarted;
+using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+ConfigureServices(builder.Services, builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+{
+    ConfigureMassTransit(services, configuration);
+}
+
+void ConfigureMassTransit(IServiceCollection services, IConfiguration configuration)
+{
+    services.AddMassTransit(x =>
+    {
+        x.SetKebabCaseEndpointNameFormatter();
+
+        // var entryAssembly = Assembly.GetEntryAssembly();
+        //x.AddSagaStateMachines(entryAssembly);
+        //x.AddSagas(entryAssembly);
+        //x.AddActivities(entryAssembly);
+
+        var useRabbitMq = configuration.GetValue<bool>("Masstransit:UseRabbitMq");
+
+        if (useRabbitMq)
+        {
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host("localhost", "/", h => {
+                    h.Username("guest");
+                    h.Password("guest");
+
+                });
+                cfg.ConfigureEndpoints(context);
+            });
+        }
+
+        else
+        {
+            x.UsingInMemory((context, cfg) =>
+            {
+                cfg.ConfigureEndpoints(context);
+            });
+        }
+
+        // processors
+        services.AddHostedService<Worker>();
+    });
+}
