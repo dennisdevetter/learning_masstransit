279d40f4e5bc0d670896b15e90f078810bf360f3
Dennis De Vetter <dennis.de.vetter@telenet.be>
2023-03-20 16:02:47 +0100

    wizard

--- LearningMassTransit.Consumers/Lara/WizardCreatedConsumer.cs
+++ LearningMassTransit.Consumers/Lara/WizardCreatedConsumer.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using LearningMassTransit.Contracts.Dtos;
 using LearningMassTransit.Domain;
+using LearningMassTransit.Domain.Lara;
 using LearningMassTransit.Messaging.Lara;
 using MassTransit;
+using Newtonsoft.Json;
 
 namespace LearningMassTransit.Consumers.Lara;
 
@@ -20,13 +24,33 @@ public class WizardCreatedConsumer : IConsumer<WizardCreated>
 
         var wizard = await _laraUnitOfWork.Wizards.GetById(wizardid, context.CancellationToken);
 
-        if (wizard != null)
+        switch (wizard?.Kind)
         {
-            var steps = wizard.Steps;
-            foreach (var step in steps)
-            {
+            case "AdresVoorstel":
+                HandleAdresVoorstelWizard(wizard);
+                break;
+        }
+    }
 
+    private void HandleAdresVoorstelWizard(Wizard wizard)
+    {
+        foreach (var step in wizard.Steps)
+        {
+            if (step.StepType == typeof(CreateAdresVoorstelDto).AssemblyQualifiedName)
+            {
+                HandleCreateAdresVoorstel(step);
             }
         }
+    }
+
+    private void HandleCreateAdresVoorstel(WizardStep step)
+    {
+        var data = JsonConvert.DeserializeObject<CreateAdresVoorstelDto>(step.StepData);
+
+        if (data != null)
+        {
+
+        }
+
     }
 }
