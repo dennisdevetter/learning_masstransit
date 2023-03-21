31f799d6728c523dfa66be65ec472a80e7c9c027
Dennis De Vetter <dennis.de.vetter@telenet.be>
2023-03-21 09:44:02 +0100

    wizard saga completed

---
+++ LearningMassTransit.Messaging/Lara/WizardCompleted.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LearningMassTransit.Messaging.Lara;
+
+public class WizardCompleted
+{
+    public Guid WizardId { get; set; }
+}
--- LearningMassTransit.Processor.Api/Jobs/WizzardPollingJob.cs
+++ LearningMassTransit.Processor.Api/Jobs/WizzardPollingJob.cs
@@ -1,23 +1,39 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using LearningMassTransit.Domain;
+using LearningMassTransit.Domain.Lara;
+using LearningMassTransit.Messaging.Lara;
+using MassTransit;
 using Quartz;
 
 namespace LearningMassTransit.Processor.Api.Jobs;
 
 public class WizzardPollingJob : IJob
 {
+    private readonly IBus _bus;
     private readonly ILaraUnitOfWork _laraUnitOfWork;
 
-    public WizzardPollingJob(ILaraUnitOfWork laraUnitOfWork)
+    public WizzardPollingJob(ILaraUnitOfWork laraUnitOfWork, IBus bus)
     {
+        _bus = bus;
         _laraUnitOfWork = laraUnitOfWork;
     }
-
 
     public async Task Execute(IJobExecutionContext context)
     {
         var wizards = await _laraUnitOfWork.Wizards.All(context.CancellationToken);
 
+        // polling logic to async api
 
+        var completedWizards = wizards.Where(x => x.Status == WizardStatusEnum.Completed).ToList();
+
+        if (completedWizards.Any())
+        {
+            foreach (var completedWizard in completedWizards)
+            {
+                // notify saga
+                await _bus.Publish(new WizardCompleted { WizardId = completedWizard.WizardId }, context.CancellationToken);
+            }
+        }
     }
 }
