b9a86be36de90cbdd6989195d7b6aae15759bfe8
Dennis De Vetter <dennis.de.vetter@telenet.be>
2023-03-27 19:13:26 +0200

    remove

--- LearningMassTransit.Contracts/Dtos/AdresVoorstelCreatedDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using LearningMassTransit.Contracts.Enums;
-
-namespace LearningMassTransit.Contracts.Dtos;
-
-public class AdresVoorstelCreatedDto
-{
-    public ActieEnum Actie = ActieEnum.ProposeStreetName;
-    public string TicketId { get; set; }
-}
